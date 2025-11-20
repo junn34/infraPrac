638fb84ce6dfa98ae7a68b9f7098d6059b4b28fc
junn34 <lucasorjh3412@gmail.com>
2025-11-20 10:23:02 +0900

    마지막텟트ㅡ

--- frontend/bankedu-front/src/app/page.jsx
+++ frontend/bankedu-front/src/app/page.jsx
@@ -6,11 +6,12 @@ export default function Page() {
       <div className="text-center pt-16">
         <h1 className="text-3xl font-bold text-blue-700">BankEdu</h1>
         <p className="text-gray-600 mt-2">
-          디지털 금융을 쉽고 재미있게 배우는  교육 플랫폼이지롱3444333
+          디지털 금융을 쉽고 재미있게 배우는  교육 플랫폼이지롱344ddd4333
         </p>
       </div>
       <AuthForm mode="login" />
     </main>
   );
 }
+
 
